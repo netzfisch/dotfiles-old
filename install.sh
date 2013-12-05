fa63a6c87977a75d449ff85ae65d868dfec7118c
Harm Behrens <hb@netzfisch.de>
2013-12-05 19:11:23 +0100

    customise install script for better exception-/XfceTerminal-handling
    * switch from /bin/sh to /bin/bash, because of better array handling
    * add 'license' to exceptions, which don't get linked
    * integrate different target path for Xfce4 'terminalrc'

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 for name in *; do
-  target="$HOME/.$name"
+  exceptions="install.sh LICENSE README.md"
+  
+  if [ "$name" == "terminalrc" ]; then
+    target="$HOME/.config/xfce4/terminal/$name"
+  else
+    target="$HOME/.$name"
+  fi
+
   if [ -e "$target" ]; then
     if [ ! -L "$target" ]; then
       echo "WARNING: $target exists but is not a symlink."
     fi
   else
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+    if [[ ! "${exceptions[@]}" =~ "$name" ]]; then
       echo "Creating $target"
       ln -s "$PWD/$name" "$target"
     fi
